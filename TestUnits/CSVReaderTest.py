2d7f135660050c0966d02aba7a488d27f438d25f
Moradisten <moradisten>
2020-04-07 00:59:46 +0200

    CSVReader testing...

--- TestUnits/CSVReaderTest.py
+++ TestUnits/CSVReaderTest.py
@@ -1,14 +1,61 @@
 import unittest
-from core  import CSVReader
+from core import CSVReader
+from dataset import ListDataset
+
 
 class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)
+    file_name1 = "/Users/morad/PycharmProjects/PForests/util/tabla1.csv"
+    file_name2 = "/Users/morad/PycharmProjects/PForests/util/tabla2.csv"
+    file_name3 = "/Users/morad/PycharmProjects/PForests/util/tabla3.csv"
+    file_name4 = "/Users/morad/PycharmProjects/PForests/util/tabla4.csv"
+    tabla_1 = open(file_name1)
+    tabla_2 = open(file_name2)
+    tabla_3 = open(file_name3)
+    tabla_4 = open(file_name4)
 
+    def test_read_file1(self):
 
-    def test_read_file(self):
-        fileinfo = CSVReader.CSVReader.getFileInformation("/Users/morad/texto.txt", separator=" ")
-        print(fileinfo)
+        line = self.tabla_1.readline()
+        while len(line) > 0:
+            print(line)
+            line = self.tabla_1.readline()
+
+    def test_read_file2(self):
+        line = self.tabla_2.readline()
+        while len(line) > 0:
+            print(line)
+            line = self.tabla_2.readline()
+
+    def test_read_file3(self):
+        line = self.tabla_3.readline()
+        while len(line) > 0:
+            print(line)
+            line = self.tabla_3.readline()
+
+    def test_num_columns1(self):
+        file_info = CSVReader.CSVReader.getFileInformation(filename=self.file_name1, separator=" ")
+        self.assertEqual(file_info[1], 5)
+        self.assertEqual(file_info[0], 10)
+
+    def test_num_columns2(self):
+        file_info = CSVReader.CSVReader.getFileInformation(filename=self.file_name2, has_header=True, separator=" ")
+        self.assertEqual(file_info[1], 4)
+        self.assertEqual(file_info[0], 10)
+
+    def test_num_columns4(self):
+        file_info = CSVReader.CSVReader.getFileInformation(filename=self.file_name4, has_header=True, separator=" ")
+        self.assertEqual(file_info[1], 5)
+        self.assertEqual(file_info[0], 10)
+
+    # Comprobaciones con el dataset
+
+    def test_dataset_labels(self):
+
+        dataset = CSVReader.CSVReader.readCSVToListDataset(fileName=self.file_name1, has_header=False, targetColumnsIsFirst=True, separator=" ")
+        print(len(dataset.labels))
+        for l in dataset.labels:
+            print(dataset.data[l])
+
 
 if __name__ == '__main__':
     unittest.main()
--- core/CSVReader.py
+++ core/CSVReader.py
@@ -1,13 +1,18 @@
 import io
 import time
 
+from numpy import double
+
+from dataset import ListDataset
+
 from numpy.distutils.fcompiler import none
 
 
 class CSVReader:
 
     @staticmethod
-    def getFileInformation(filename, separator=" "):
+    def getFileInformation(filename, has_header=False, separator=" "):
+        line_array = []
         file = none
         length_check = True
         num_lines = 0
@@ -18,16 +23,64 @@ class CSVReader:
             print("[ERROR]", "File", filename, "Not Found")
             return
         line = file.readline()
+        length_check = True
         while len(line) != 0:
             num_lines = num_lines + 1
-            if num_columns < len(line.split(separator)):
-                num_columns = len(line.split(separator))
+            if length_check:
+                length_check = False
+                line_array = line.split(separator)
             line = file.readline()
+
+        if has_header:
+            if num_lines > 0:
+                num_lines = num_lines - 1
+        num_columns = len(line_array)
+        file.close()
         return num_lines, num_columns
 
     @staticmethod
-    def readCSVToListDataset(fileName, targetColumnsIsFirst, separator):
-        file = open(fileName, "r")
-        print("Reading File:[", fileName, "]")
+    def readCSVToListDataset(fileName, has_header, targetColumnsIsFirst=True, separator=" "):
+        file = none
+        try:
+            file = open(fileName, "r")
+            print("Reading File:[", fileName, "]")
+        except:
+            print("File Not Found:[", fileName, "]")
+            return
+        start = time.time()
+        file_info = CSVReader.getFileInformation(fileName, has_header, separator)
+        expected_size = file_info[0]
+        data_length = file_info[1] - 1
+        dataset = ListDataset.ListDataset(expected_size, 0)
 
-        return
+        line = file.readline()
+        num_line = 0
+        while len(line) > 0:
+            line_array = line.split(separator)
+            series = list()
+            if targetColumnsIsFirst:
+                init_index = 1
+            else:
+                init_index = 0
+            for j in range(init_index, len(line_array)):
+                series.append(double(line_array[j]))
+            try:
+                if init_index == 1:
+                    label = line_array[0]
+                else:
+                    label = num_line
+            except:
+                label = none
+
+            if label != none:
+                dataset.add_series(label, series)
+            num_line = num_line + 1
+            line = file.readline()
+
+            # TODO: IMPLEMENT MEMORY STATS
+
+        end = time.time()
+        elapsed = end - start
+        print("finished in", elapsed, "seconds")
+        file.close()
+        return dataset
