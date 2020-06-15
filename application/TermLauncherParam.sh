a747e35405b6ff9e201641e3a5b708d5ed155465
Moradisten <moradisten>
2020-06-15 19:52:13 +0200

    [FASE 2] Preparing launcher

--- application/TermLauncherParam.sh
+++
@@ -1,6 +0,0 @@
-#python AppRunner.py -name=MoteStrain -train=/Users/morad/Desktop/resultados/MoteStrain/MoteStrain_TRAIN.arff -test=/Users/morad/Desktop/resultados/MoteStrain/MoteStrain_TRAIN.arff -repeat=1 -trees=100 -candidates=5 -targetlast=True -calculate=accuracy
-#python AppRunner.py -name=ItalyPower -train=/Users/morad/Desktop/resultados/ItalyPowerDemand/ItalyPowerDemand_TRAIN.arff -test=/Users/morad/Desktop/resultados/ItalyPowerDemand/ItalyPowerDemand_TEST.arff -repeat=1 -trees=100 -candidates=5 -targetlast=True -calculate=accuracy
-#python AppRunner.py -name=ItalyPowerDemand -train=/Users/morad/PycharmProjects/PForests/datasets/ItalyPowerDemand/ItalyPowerDemand_TRAIN.arff -test=/Users/morad/PycharmProjects/PForests/datasets/ItalyPowerDemand/ItalyPowerDemand_TEST.arff -repeat=1 -trees=100 -candidates=5 -targetlast=True -calculate=accuracy
-#python AppRunner.py -name=ItalyPowerDemand -train=/Users/morad/Downloads/ItalyPowerDemand/ItalyPowerDemand_TRAIN.csv -test=/Users/morad/Downloads/ItalyPowerDemand/ItalyPowerDemand_TEST.csv -repeat=1 -trees=100 -candidates=5 -targetlast=True -calculate=all
-DATASET_DIR=$1$2/
-python AppRunner.py -name=$2 -train=$DATASET_DIR$2_TRAIN.arff -test=$DATASET_DIR$2_TEST.arff -repeat=1 -trees=100 -candidates=5 -targetlast=True -calculate=accuracy
--- launchers/TermLauncherParam.sh
+++ launchers/TermLauncherParam.sh
@@ -2,5 +2,6 @@
 #python AppRunner.py -name=ItalyPower -train=/Users/morad/Desktop/resultados/ItalyPowerDemand/ItalyPowerDemand_TRAIN.arff -test=/Users/morad/Desktop/resultados/ItalyPowerDemand/ItalyPowerDemand_TEST.arff -repeat=1 -trees=100 -candidates=5 -targetlast=True -calculate=accuracy
 #python AppRunner.py -name=ItalyPowerDemand -train=/Users/morad/PycharmProjects/PForests/datasets/ItalyPowerDemand/ItalyPowerDemand_TRAIN.arff -test=/Users/morad/PycharmProjects/PForests/datasets/ItalyPowerDemand/ItalyPowerDemand_TEST.arff -repeat=1 -trees=100 -candidates=5 -targetlast=True -calculate=accuracy
 #python AppRunner.py -name=ItalyPowerDemand -train=/Users/morad/Downloads/ItalyPowerDemand/ItalyPowerDemand_TRAIN.csv -test=/Users/morad/Downloads/ItalyPowerDemand/ItalyPowerDemand_TEST.csv -repeat=1 -trees=100 -candidates=5 -targetlast=True -calculate=all
-DATASET_DIR=/Users/morad/PycharmProjects/PForests/datasets/$2/
-python AppRunner.py -name=$2 -train=$DATASET_DIR/$2_TRAIN.arff -test=$DATASET_DIR/$2_TEST.arff -repeat=1 -trees=100 -candidates=5 -targetlast=True -calculate=accuracy
+CUR_DIR=$(cd .. && pwd)
+DATASET_DIR=$CUR_DIR/datasets/$1/
+python ../application/AppRunner.py $CUR_DIR -name=$1 -train=$DATASET_DIR/$1_TRAIN.arff -test=$DATASET_DIR/$1_TEST.arff -repeat=1 -trees=100 -candidates=5 -targetlast=True -calculate=accuracy
