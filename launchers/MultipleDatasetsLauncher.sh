05663d2ea794d41ca39c5b190e95073392adceb8
Moradisten <moradisten>
2020-06-28 17:33:21 +0200

    UpdatingComparator Launchers

--- launchers/MultipleDatasetsLauncher.sh
+++ launchers/MultipleDatasetsLauncher.sh
@@ -5,8 +5,8 @@ for d in $CUR_DIR/datasets/*; do
     DATASET_DIR=$CUR_DIR/datasets/$dataset/
     echo ""
     echo "Executing dataset [ $dataset ]  ..."
-    python3 ../application/AppRunner.py $CUR_DIR -name=$dataset -train=$DATASET_DIR/"$dataset"_TRAIN.arff -test=$DATASET_DIR/"$dataset"_TEST.arff -repeat=1 -trees=100 -candidates=5 -targetlast=True -calculate=accuracy
-    echo "Classification process finished"
+    python3 ../application/AppRunner.py $CUR_DIR -name=$dataset -train=$DATASET_DIR/"$dataset"_TRAIN.csv -test=$DATASET_DIR/"$dataset"_TEST.csv -repeat=1 -trees=100 -candidates=5 -targetlast=True -calculate=accuracy
+    echo "Classification process [ $dataset ] finished"
   fi
 done
 
--- launchers/launchComparator.sh
+++ launchers/launchComparator.sh
@@ -1,3 +1,13 @@
 CUR_DIR=$(cd .. && pwd)
-DATASET_DIR=$CUR_DIR/datasets/$1/
-python3 ../application/AppRunner.py $CUR_DIR -name=$1 -train=$DATASET_DIR/$1_TRAIN.csv -test=$DATASET_DIR/$1_TEST.csv -repeat=1 -trees=100 -candidates=5 -targetlast=True -ignoreFirst=False
+for d in $CUR_DIR/datasets/*; do
+  if [ -d $d ]; then
+    dataset=$(basename $d)
+    DATASET_DIR=$CUR_DIR/datasets/$dataset/
+    echo ""
+    echo "Executing dataset [ $dataset ]  ..."
+    python3 ../application/AppRunner.py $CUR_DIR -name=$dataset -train=$DATASET_DIR/"$dataset"_TRAIN.csv -test=$DATASET_DIR/"$dataset"_TEST.csv -repeat=1 -trees=100 -candidates=5 -targetlast=True -calculate=accuracy
+    java -jar -Xmx1g ../application/ProximityForest.jar  -train=$DATASET_DIR/"$dataset"_TRAIN.csv -test=$DATASET_DIR/"$dataset"_TEST.csv -repeats=1 -trees=100 -r=5 -edistance=dtw
+    echo "Classification process [ $dataset ] finished"
+  fi
+done
+
