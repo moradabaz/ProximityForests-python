f683b45a1baea3819949918eea5661fe2a75f86f
Moradisten <moradisten>
2020-06-28 19:09:18 +0200

    nuevo launcher comparador

---
+++ launchers/launchComparator2.sh
@@ -0,0 +1,14 @@
+CUR_DIR=$(cd .. && pwd)
+for d in $CUR_DIR/datasets/*; do
+  if [ -d $d ]; then
+    dataset=$(basename $d)
+    DATASET_DIR=$CUR_DIR/datasets/$dataset/
+    echo ""
+    echo "Executing dataset [ $dataset ]  ..."
+    python3 ../application/AppRunner.py $CUR_DIR -name=$dataset -train=$DATASET_DIR/"$dataset"_TRAIN.csv -test=$DATASET_DIR/"$dataset"_TEST.csv -repeat=1 -trees=100 -candidates=5 -targetlast=True -calculate=accuracy
+    python3 ../application/AppRunner.py $CUR_DIR -name=$dataset -train=$DATASET_DIR/"$dataset"_TRAIN.csv -test=$DATASET_DIR/"$dataset"_TEST.csv -repeat=1 -trees=10 -candidates=2 -targetlast=True -calculate=accuracy
+
+    echo "Classification process [ $dataset ] finished"
+  fi
+done
+
