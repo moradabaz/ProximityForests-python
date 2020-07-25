ebc0add2f095eeefbe7580de9968e14620fd5cab
Moradisten <moradisten>
2020-07-25 15:19:38 +0200

    AFSD

--- launchers/TermLauncherParam.sh
+++ launchers/TermLauncherParam.sh
@@ -3,5 +3,13 @@
 #python AppRunner.py -name=ItalyPowerDemand -train=/Users/morad/PycharmProjects/PForests/datasets/ItalyPowerDemand/ItalyPowerDemand_TRAIN.arff -test=/Users/morad/PycharmProjects/PForests/datasets/ItalyPowerDemand/ItalyPowerDemand_TEST.arff -repeat=1 -trees=100 -candidates=5 -targetlast=True -calculate=accuracy
 #python AppRunner.py -name=ItalyPowerDemand -train=/Users/morad/Downloads/ItalyPowerDemand/ItalyPowerDemand_TRAIN.csv -test=/Users/morad/Downloads/ItalyPowerDemand/ItalyPowerDemand_TEST.csv -repeat=1 -trees=100 -candidates=5 -targetlast=True -calculate=all
 CUR_DIR=$(cd .. && pwd)
-DATASET_DIR=$CUR_DIR/datasets/$1/
-python3 ../application/PFApplication.py $CUR_DIR -name=$1 -train=$DATASET_DIR/$1_TRAIN.arff -test=$DATASET_DIR/$1_TEST.arff -repeat=1 -trees=100 -candidates=5 -targetlast=True -calculate=accuracy
+for d in $CUR_DIR/datasets/*; do
+  if [ -d $d ]; then
+    dataset=$(basename $d)
+    DATASET_DIR=$CUR_DIR/datasets/$dataset/
+    CUR_DIR=$(cd .. && pwd)
+    DATASET_DIR=$CUR_DIR/datasets/$dataset/
+    python3 ../application/PFApplication.py $CUR_DIR -name=$dataset -train=$DATASET_DIR/"$dataset"_TRAIN.arff -test=$DATASET_DIR/"$dataset"_TEST.arff -targetlast=True -calculate=accuracy
+    echo "Classification process [ $dataset ] finished"
+  fi
+done
