9e28089f7671cfb9f9228d10b9f36d87fe6b218a
Moradisten <moradisten>
2020-07-30 20:53:08 +0200

    new crossvalidation

--- application/crossvalidation.py
+++ application/crossvalidation.py
@@ -3,8 +3,8 @@ import json
 import time
 import random
 from scipy.io import arff
-
 sys.path.append(sys.argv[1])
+from core import AppContext
 from core.ExperimentRunner import ExperimentRunner
 from core.FileReader import FileReader
 
@@ -13,17 +13,11 @@ name = sys.argv[2]
 
 
 def save_json(folds):
-    f_path = '../outputs/' + name + '_Folds_' + str(time.localtime().tm_hour) + str(time.localtime().tm_min) + str(
-        time.localtime().tm_sec) + ".json"
-    data = {'fold_stats': []}
-    for fold in folds:
-        data_stats = folds[fold]._exportJSONstats()
-        data['fold_stats'].append({
-            'name': name,
-            'Fold_' + str(fold): data_stats
-        })
+    f_path = '../outputs/' + name + '_Folds.json'
+    header = 'iterations,trees,candidates,accuracy'
     with open(f_path, 'w+') as file:
-        file.write(json.dumps(data))
+        file.writelines("%s\n" % header)
+        file.writelines("%s\n" % linea for linea in folds)
     file.close()
     return
 
@@ -50,19 +44,23 @@ for i in range(0, num_series):
         folds[key].append(arff_data[i])
 train_data = list()
 fold_accuracies = dict()
+lineas = list()
 runner = ExperimentRunner()
-for turn in folds.keys():
-    if turn == 1:
-        continue
-    test_dataset = FileReader.parse_arff_data(folds[turn])
-    for fold in folds.keys():
-        if fold < turn:
-            for line in folds[fold]:
-                train_data.append(line)
+AppContext.AppContext.num_repeats = 10
+for trees in [20, 35, 50, 75, 100, 120]:
+    AppContext.AppContext.num_trees = trees
+    for candidates in [2, 3, 4, 5, 6, 7]:
+        for turn in folds.keys():
+            test_dataset = FileReader.parse_arff_data(folds[turn])
+            for fold in folds.keys():
+                if fold != turn:
+                    for line in folds[fold]:
+                        train_data.append(line)
 
-    print("FOLD ", turn, " ->")
-    train_dataset = FileReader.parse_arff_data(train_data)
-    pforest = runner.run_data(train_dataset, test_dataset, name)
-    fold_accuracies[turn] = pforest.result
-
-save_json(fold_accuracies)
+            print("FOLD ", turn, " ->")
+            train_dataset = FileReader.parse_arff_data(train_data)
+            pforest = runner.run_data(train_dataset, test_dataset, name)
+            fold_accuracies[turn] = pforest.result.accuracy
+            linea = str(turn) + ',' + str(trees) + ',' + str(candidates) + ',' + str(fold_accuracies[turn])
+            lineas.append(linea)
+save_json(lineas)
